edef24d1a1023aef39824b14fb3ef02e5d2cb82d
Samuel Nettey <snettey@hotmail.de>
2020-05-25 17:05:46 +0200

    initial spike

--- src/Outlook Manager/Outlook Manager/OutlookMail.cs
+++ src/Outlook Manager/Outlook Manager/OutlookMail.cs
@@ -1,7 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Office.Interop.Outlook;
+
 namespace Outlook_Manager
 {
     public class OutlookMail
     {
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
         
+        public static  List<OutlookMail> ReadMails()
+        {
+            Application outlookApplication = null;
+            NameSpace outlookNameSpace = null;
+            MAPIFolder inboxFolder = null;
+            Items items = null;
+            List<OutlookMail> emails = new List<OutlookMail>();
+            OutlookMail mail;
+
+            try
+            {
+                outlookApplication = new Application();
+                outlookNameSpace = outlookApplication.GetNamespace("MAPI");
+                inboxFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+                items = inboxFolder.Items;
+
+                foreach (MailItem item in items)
+                {
+                    emails.Add(new OutlookMail
+                    {
+                        From = item.SenderEmailAddress,
+                        Subject = item.Subject,
+                        Body = item.Body
+                    });
+                    ReleaseComObject(item);
+                }
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {   
+                ReleaseComObject(items);
+                ReleaseComObject(inboxFolder);
+                ReleaseComObject(outlookNameSpace);
+                ReleaseComObject(outlookApplication);
+            }
+
+            return emails;
+        }
+
+        private static void ReleaseComObject(object obj)
+        {
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+            obj = null;
+        }
     }
 }
---
+++ src/Outlook Manager/Outlook Manager/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Outlook_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mails = OutlookMail.ReadMails();
+
+            int i = 0;
+            foreach (var mail in mails)
+            {
+                Console.WriteLine("Mail No "+  i);
+                Console.WriteLine("Received From "+ mail.From );
+                Console.WriteLine("Mail Subject "+  mail.Subject);
+                Console.WriteLine("Mail Body "+  mail.Body);
+                Console.WriteLine("");
+                i++;
+            }
+        }
+    }
+}
